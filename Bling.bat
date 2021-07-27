d81f9cb9965243f7f1b17e6c03b5cf92653ee0b2
alpha2x <hexagonwin@disroot.org>
2021-07-28 07:09:50 +0900

    Update Bling.bat

--- Bling.bat
+++ Bling.bat
@@ -1,9 +1,23 @@
 @echo off
-echo BrowserLing Kill Script by HexagonWin
-echo Do not use for bad purpose
+echo ██████  ██      ██ ███    ██  ██████        
+echo ██   ██ ██      ██ ████   ██ ██       ▄ ██ ▄
+echo ██████  ██      ██ ██ ██  ██ ██   ███  ████ 
+echo ██   ██ ██      ██ ██  ██ ██ ██    ██ ▀ ██ ▀
+echo ██████  ███████ ██ ██   ████  ██████        
+echo 
+echo Bling* - a BrowserLing Kill script
+echo by HexagonWin, revision 4-2min
+echo Do not use for bad purpose, for educational purpose only!
+
+echo Initiate script, killing processes
 taskkill /f /im hugewin.exe
 taskkill /f /im newlauncher.exe
 taskkill /f /im taskbar.exe
-taskkill /f /im letmein.exe
 echo Done!
+echo =============================
+echo == RUNNING TASKMANAGER NOW ==
+echo == KILL letmein.exe        ==
+echo == NEEDED TO SHOW TASKBAR  ==
+echo =============================
+start taskmgr
 pause
